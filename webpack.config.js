c0bf9293a74a5a6643be2ad381533e4cc6f79d9c
Tommyduong23 <tommyduong23@gmail.com>
2018-08-14 10:50:48 -0700

    update package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require( 'path' );
 // Add dependancies to exclode in externals.
 // Ex externals : { moment: 'moment' }
 module.exports = {
-  entry: path.resolve( __dirname, 'src/main.js' ),
+  entry: path.resolve( __dirname, 'src/main.vue' ),
   output: {
-    filename: '[MODULE_NAME].js',
+    filename: 'youtubeVideo.js',
 		path: path.resolve( __dirname, 'dist' ),
-		library: '[MODULE_NAME]',
+		library: 'youtubeVideo',
 		libraryTarget: 'umd',
 		umdNamedDefine: true
   },
